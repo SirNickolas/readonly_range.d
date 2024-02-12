00f9abb9b51dd3a3d87ba00bed1cec0d0ff7157f
Nickolay Bukreyev <buknik95@yandex.ru>
2024-02-13 05:45:42 +0700

    Implement `RoRange`

--- src/ro_slice.d
+++ src/ro_slice.d
@@ -1,1 +1,136 @@
 module ro_slice;
+
+///
+public import std.range.primitives: back, empty, front, popBack, popFront, save;
+
+import std.range.primitives: isInputRange;
+
+/++
+    `true` if `R` is an input range with read-only elements. This is *not* simply the inverse of
+    [hasAssignableElements](https://dlang.org/phobos/std_range_primitives.html#hasAssignableElements):
+    when autodecoding is enabled, the latter returns `false` for narrow strings (`char[ ]`,
+    `wchar[ ]`), although they can be mutated by index (`s[i] = c`). This trait, on the other hand,
+    checks `front`, `back`, and `opIndex` independently.
++/
+enum bool hasReadonlyElements(R) =
+    isInputRange!R &&
+    !is(typeof((R r) { r.front = typeof(r.front).init; })) &&
+    !is(typeof((R r) { r.back = typeof(r.back).init; })) &&
+    !is(typeof((R r) { r[0] = typeof(r[0]).init; }));
+
+private struct _Indices(A, B) {
+    A a;
+    B b;
+}
+
+/++
+    A thin wrapper that makes the range `R` head-const (aka read-only, aka final):
+    [hasReadonlyElements]`!(RoRange!R)` is `true` for any `R`. If `R` is already head-const,
+    `RoRange` aliases itself to it.
++/
+struct RoRange(R) if (isInputRange!R && !hasReadonlyElements!R) {
+pragma(inline, true):
+    import std.range.primitives: ElementType, hasLength, isBidirectionalRange, isForwardRange;
+
+    private R _r;
+
+    /++
+        Range interface. `front`, `back`, and `opIndex` return a **copy** of the element, therefore,
+        they are not lvalues.
+    +/
+    static if (!is(typeof({ enum e = R.empty; })))
+        @property bool empty() { return _r.empty; }
+    else
+        enum empty = R.empty;
+
+    /// ditto
+    @property ElementType!R front() { return _r.front; }
+
+    /// ditto
+    void popFront() { _r.popFront(); }
+
+    static if (isBidirectionalRange!R) {
+        /// ditto
+        @property ElementType!R back() { return _r.back; }
+
+        /// ditto
+        void popBack() { _r.popBack(); }
+    }
+
+    static if (isForwardRange!R) {
+        /// ditto
+        @property RoRange!R save() { return RoRange!R(_r.save); }
+    }
+
+    static if (hasLength!R) {
+        /// ditto
+        @property size_t length() { return _r.length; }
+    }
+
+    /// ditto
+    template opDollar(size_t i) {
+        // https://dlang.org/spec/operatoroverloading.html#dollar
+        static if (is(typeof(_r.opDollar!i)))
+            auto ref opDollar() { return _r.opDollar!i; }
+        else {
+            static assert(!i, '`' ~ R.stringof ~ "` does not support multidimensional indexing");
+            static if (is(typeof(_r.opDollar)))
+                auto ref opDollar() { return _r.opDollar; }
+            else
+                alias opDollar = length;
+        }
+    }
+
+    /// ditto
+    auto ref opSlice(size_t i, A, B)(auto ref A lower, auto ref B upper) {
+        /+
+            Normally, `a[ ]` is rewritten to `a.opIndex()`, and `a[i .. j]` is rewritten to either
+            `a.opIndex(a.opSlice!0(i, j))` or `a.opIndex(a.opSlice(i, j))` if the former does not
+            compile. However, if they still fail to compile, DMD tries legacy rewrites:
+            `a[ ] -> a.opSlice()` and `a[i .. j] -> a.opSlice(i, j)`. In these cases, `opSlice` has
+            a completely different meaning. Therefore, we never invoke `_r.opSlice` manually for 0-
+            and 1-dimensional slicing since we cannot know what semantics it will have.
+
+            https://dlang.org/spec/operatoroverloading.html#slice
+        +/
+        static if (i)
+            return _r.opSlice!i(lower, upper);
+        else
+            return _Indices!(A, B)(lower, upper);
+    }
+
+    /// ditto
+    auto opIndex(Args...)(auto ref Args indices) {
+        // See the comment for `opSlice` above.
+        static if (!Args.length)
+            return RoRange!R(_r[ ]);
+        else static if (is(Args[0] == _Indices!(A, B), A, B))
+            static if (Args.length == 1) // Unfortunately, this check is required.
+                return readonly(_r[indices[0].a .. indices[0].b]);
+            else
+                return readonly(_r[indices[0].a .. indices[0].b, indices[1 .. $]]);
+        else
+            return _r[indices];
+    }
+
+    /// `RoRange!R` is implicitly convertible to `const R` (i.e., head-const to deep-const).
+    T opCast(T)() const if (is(T == const R)) { return _r; }
+
+    /// ditto
+    alias toConst = opCast!(const R);
+    alias toConst this;
+}
+
+/// ditto
+template RoRange(R) if (hasReadonlyElements!R) {
+    alias RoRange = R;
+}
+
+/// ditto
+pragma(inline, true)
+RoRange!R readonly(R)(R r) if (isInputRange!R) {
+    static if (hasReadonlyElements!R)
+        return r;
+    else
+        return RoRange!R(r);
+}
--- test/test.d
+++ test/test.d
@@ -1,1 +1,124 @@
 import ro_slice;
+
+nothrow pure @safe @nogc:
+
+unittest {
+    static assert(hasReadonlyElements!(const(int)[ ]));
+    static assert(hasReadonlyElements!(const(char)[ ]));
+    static assert(!hasReadonlyElements!(int[ ]));
+    static assert(!hasReadonlyElements!(char[ ]));
+}
+
+unittest {
+    static assert(is(RoRange!string == string));
+    static assert(is(RoRange!(char[ ]) == struct));
+    static assert(!__traits(compiles, RoRange!char));
+}
+
+unittest {
+    int[3] data = [1, 2, 3];
+    auto a = data[ ].readonly;
+    assert(!a.empty);
+    assert(a.length == 3);
+    assert(a.front == 1);
+    assert(a.back == 3);
+    assert(a[$ - 2] == 2);
+    assert(a[ ] == data[ ]);
+    assert(a <= a);
+
+    static assert(is(typeof(a[ ]) == RoRange!(int[ ])));
+    static assert(is(typeof(a[0 .. 0]) == RoRange!(int[ ])));
+
+    static assert(!__traits(compiles, a.front = 0));
+    static assert(!__traits(compiles, a.back = 0));
+    static assert(!__traits(compiles, { a[1] = 0; }));
+    static assert(!__traits(compiles, ++a[1]));
+    static assert(!__traits(compiles, a[1]++));
+    static assert(!__traits(compiles, { a[ ] = 0; }));
+    static assert(!__traits(compiles, ++a[ ]));
+    static assert(!__traits(compiles, a[ ]++));
+    static assert(!__traits(compiles, { a[ ][ ] = 0; }));
+    static assert(!__traits(compiles, ++a[ ][ ]));
+    static assert(!__traits(compiles, a[ ][ ]++));
+    static assert(!__traits(compiles, { a[0 .. 0] = 0; }));
+    static assert(!__traits(compiles, ++a[0 .. 0]));
+    static assert(!__traits(compiles, a[0 .. 0]++));
+    static assert(!__traits(compiles, { a[0 .. 0][ ] = 0; }));
+    static assert(!__traits(compiles, ++a[0 .. 0][ ]));
+    static assert(!__traits(compiles, a[0 .. 0][ ]++));
+}
+
+unittest {
+    struct Point {
+        int x, y;
+    }
+
+    Point[1] data = [
+        { x: 1, y: 2 },
+    ];
+    auto p = data[ ].readonly;
+    static assert(hasReadonlyElements!(typeof(p)));
+}
+
+unittest {
+    import std.algorithm.iteration: filter;
+    import std.range: iota;
+    import std.range.primitives: isRandomAccessRange;
+
+    auto a = iota(10).filter!q{a & 0x1};
+    static assert(!isRandomAccessRange!(typeof(a)));
+    static assert(is(typeof(a.readonly) == typeof(a)));
+}
+
+unittest {
+    import std.range.primitives;
+
+    static struct Forever {
+        struct Dollar { }
+        struct InfiniteSlice { }
+
+        int* p;
+
+        enum empty = false;
+        @property ref inout(int) front() inout { return *p; }
+        void popFront() const { }
+        @property inout(Forever) save() inout { return this; }
+
+        enum opDollar = Dollar.init;
+        size_t[2] opSlice(size_t dim: 0)(size_t i, size_t j) const { return [i, j]; }
+        InfiniteSlice opSlice(size_t dim: 0)(size_t, Dollar) const { return InfiniteSlice.init; }
+
+        ref inout(int) opIndex(size_t) inout { return *p; }
+        inout(Forever) opIndex() inout { return this; }
+        inout(Forever) opIndex(InfiniteSlice) inout { return this; }
+
+        auto opIndex(size_t[2] indices) {
+            import std.range: takeExactly;
+
+            return this.takeExactly(indices[1] - indices[0]);
+        }
+    }
+
+    static assert(isRandomAccessRange!Forever);
+    static assert(!hasReadonlyElements!Forever);
+
+    int x = 5;
+    auto a = Forever((() @trusted => &x)()).readonly;
+    alias R = typeof(a);
+    static assert(isForwardRange!R);
+    static assert(isRandomAccessRange!R);
+    static assert(isInfinite!R);
+    static assert(hasSlicing!R);
+    static assert(hasReadonlyElements!R);
+    static assert(!hasLength!R);
+    a = a[1 .. $];
+    assert(a.front == 5);
+    x++;
+    assert(a.front == 6);
+}
+
+unittest {
+    char[5] msg = "hello";
+    auto a = msg[ ].readonly;
+    static assert(!__traits(compiles, { a[4] = '!'; }));
+}
